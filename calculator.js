ad4dceae96b1ed980a638a65c50943a02078db03
Farzeen Rashid <farzeenrashid2004@gmail.com>
2024-04-29 18:34:04 -0700

    Add text and styles to all buttons

--- calculator.js
+++ calculator.js
@@ -1,12 +1,27 @@
 document.addEventListener('DOMContentLoaded', function() {
+    let countx = 7;
+    let county = 4;
+    let countz = 1;
     const buttons = document.getElementById("buttons");
     for (let i = 0; i < 4; i++) {
         for (let j = 0; j < 4; j++) {
             const cell = document.createElement("div");
             cell.classList.add("cell");
             buttons.appendChild(cell);
+            if ((i == 0 && j == 0) || (i == 0 && j == 1) || (i == 0 && j == 2))
+            { cell.textContent = countx++; }
+            if ((i == 1 && j == 0) || (i == 1 && j == 1) || (i == 1 && j == 2))
+            { cell.textContent = county++; }
+            if ((i == 2 && j == 0) || (i == 2 && j == 1) || (i == 2 && j == 2))
+            { cell.textContent = countz++; }
+            if (i == 3 && j == 0) { cell.textContent = "0"; }
+            if (i == 3 && j == 1) { cell.textContent = "."; }
+            if (i == 3 && j == 2) { cell.textContent = "="; cell.style.backgroundColor = "#692A33"; cell.style.boxShadow = "0px 4px 0px #3a1b1b";}
+            if (i == 0 && j == 3) { cell.textContent = "÷"; cell.style.backgroundColor = "#4d84ab"; cell.style.boxShadow = "0px 4px 0px #2d3653";}
+            if (i == 1 && j == 3) { cell.textContent = "×"; cell.style.backgroundColor = "#4d84ab"; cell.style.boxShadow = "0px 4px 0px #2d3653";}
+            if (i == 2 && j == 3) { cell.textContent = "-"; cell.style.backgroundColor = "#4d84ab"; cell.style.boxShadow = "0px 4px 0px #2d3653";}
+            if (i == 3 && j == 3) { cell.textContent = "+"; cell.style.backgroundColor = "#4d84ab"; cell.style.boxShadow = "0px 4px 0px #2d3653";}
         }
     }
 });
-
 
